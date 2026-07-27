efad85f27c1c95197f1a1f6ba6c4b6f078f8e8a5
桜川夢璃 <Yume@YumeYuka.moe>
2026-07-27 20:41:20 +0800

    (release): bump version to v0.6.0 and update moe quote

--- data/src/data/store/AppSettingsStore.kt
+++ data/src/data/store/AppSettingsStore.kt
@@ -45,7 +45,7 @@ class AppSettingsStore(externalMmkv: MMKV) : MMKVPreference(externalMmkv = exter
     val moeWallpaperZoom by floatFlow(1.0f)
     val moeWallpaperBiasX by floatFlow(0.0f)
     val moeWallpaperBiasY by floatFlow(0.0f)
-    val moeHomeQuote by strFlow("悪いことしたの？いけなかったの？")
+    val moeHomeQuote by strFlow("さよならの朝指きりこのまま離れないで")
     val moeHomeQuoteAuthor by strFlow("恋文")
     val moeSidebarExpanded by boolFlow(true)
     val pageScale by floatFlow(1.0f)
